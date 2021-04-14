dc65f0d8c715d5514f0ab13969e98f8c229680a3
Syr7-s <isasayar7@github.com>
2021-04-14 17:50:25 +0300

    attributes and toUser method were added to the UserDto

--- src/main/java/com/syrisa/springlibrarydocker/dto/UserDto.java
+++ src/main/java/com/syrisa/springlibrarydocker/dto/UserDto.java
@@ -1,4 +1,32 @@
 package com.syrisa.springlibrarydocker.dto;
 
+
+import com.syrisa.springlibrarydocker.model.impl.User;
+import com.syrisa.springlibrarydocker.utility.gender.Gender;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+@Data
+@Builder
 public class UserDto {
+    private long userID;
+    private String userName;
+    private String userLastName;
+    private Gender userGender;
+    private String userPhone;
+    private String userEmail;
+    private LocalDate userBirthDate;
+
+    public User toUser(){
+        return User.builder()
+                .userID(this.userID)
+                .userName(this.userName)
+                .userLastName(this.userLastName)
+                .userGender(this.userGender)
+                .userPhone(this.userPhone)
+                .userEmail(this.userEmail)
+                .userBirthDate(userBirthDate)
+                .build();
+    }
 }
