f681f680c759316260498c59b51de01a9bb1c480
Syr7-s <isasayar7@github.com>
2021-04-14 18:05:22 +0300

    attributes and toAddress method were added to the AddressDto class

--- src/main/java/com/syrisa/springlibrarydocker/dto/AddressDto.java
+++ src/main/java/com/syrisa/springlibrarydocker/dto/AddressDto.java
@@ -1,4 +1,25 @@
 package com.syrisa.springlibrarydocker.dto;
 
+import com.syrisa.springlibrarydocker.model.impl.Address;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class AddressDto {
+    private int addressID;
+    private String street;
+    private String number;
+    private String city;
+    private int zipcode;
+
+    public Address toAddress(){
+        return Address.builder()
+                .addressID(this.addressID)
+                .street(this.street)
+                .number(this.number)
+                .city(this.city)
+                .zipcode(this.zipcode)
+                .build();
+    }
 }
