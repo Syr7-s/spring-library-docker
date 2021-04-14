419a19ce7baa9bcbf73d2ae5d49329b43268e7a0
Syr7-s <isasayar7@github.com>
2021-04-14 17:45:53 +0300

    properties were added.

--- src/main/java/com/syrisa/springlibrarydocker/model/impl/User.java
+++ src/main/java/com/syrisa/springlibrarydocker/model/impl/User.java
@@ -1,4 +1,34 @@
 package com.syrisa.springlibrarydocker.model.impl;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.syrisa.springlibrarydocker.utility.gender.Gender;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+
+import java.time.LocalDate;
+
+@Entity
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class User {
+    @Id
+    private long userID;
+    private String userName;
+    private String userLastName;
+    @Enumerated(EnumType.STRING)
+    private Gender userGender;
+    @Pattern(regexp = "^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\\s\\./0-9]*$", message = "Phone Number Ex:+(123)-456-78-90")
+    @Length(min = 18, max = 18)
+    private String userPhone;
+    @Pattern(regexp = "^(.+)@(.+)$", message = "email is invalid.")
+    private String userEmail;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate userBirthDate;
 }
