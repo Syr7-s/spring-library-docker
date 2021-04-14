e742d728208477f3284d4eafc24c78727101f178
Syr7-s <isasayar7@github.com>
2021-04-14 16:18:48 +0300

    initial commit

---
+++ src/main/java/com/syrisa/springlibrarydocker/SpringLibraryDockerApplication.java
@@ -0,0 +1,13 @@
+package com.syrisa.springlibrarydocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLibraryDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLibraryDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/syrisa/springlibrarydocker/SpringLibraryDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syrisa.springlibrarydocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLibraryDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
