2cdc2acdc8b1d1bd178fcf5bd05a6a109e0d621e
Syr7-s <isasayar7@github.com>
2021-04-14 18:00:47 +0300

    attributes were added to the Address class

--- src/main/java/com/syrisa/springlibrarydocker/model/impl/Address.java
+++ src/main/java/com/syrisa/springlibrarydocker/model/impl/Address.java
@@ -1,4 +1,22 @@
 package com.syrisa.springlibrarydocker.model.impl;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int addressID;
+    private String street;
+    private String number;
+    private String city;
+    private int zipcode;
 }
