f914ec7fd27636396d9a83a05077a26f96fd0069
Syr7-s <isasayar7@github.com>
2021-04-18 15:37:48 +0300

    findCategoryByCategoryName and delete methods were added.

--- src/main/java/com/syrisa/springlibrarydocker/service/CategoryService.java
+++ src/main/java/com/syrisa/springlibrarydocker/service/CategoryService.java
@@ -1,4 +1,9 @@
 package com.syrisa.springlibrarydocker.service;
 
+import com.syrisa.springlibrarydocker.model.impl.Category;
+
 public interface CategoryService {
+    Category findCategoryByCategoryName(String categoryName);
+
+    String delete(int id);
 }
