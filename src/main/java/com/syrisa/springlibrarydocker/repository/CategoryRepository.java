e59fd5a4e3a5bad207edb54e95e5de59c5407e36
Syr7-s <isasayar7@github.com>
2021-04-18 15:35:18 +0300

    findCategoryByCategoryName method was added.

--- src/main/java/com/syrisa/springlibrarydocker/repository/CategoryRepository.java
+++ src/main/java/com/syrisa/springlibrarydocker/repository/CategoryRepository.java
@@ -4,6 +4,8 @@ import com.syrisa.springlibrarydocker.model.impl.Category;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
-public interface CategoryRepository extends CrudRepository<Category,Integer> {
+public interface CategoryRepository extends CrudRepository<Category, Integer> {
+    Category findCategoryByCategoryName(String categoryName);
 }
