c6157e427f273dce47ed852643b8e0e9d4e2dcbd
Syr7-s <isasayar7@github.com>
2021-04-22 17:08:10 +0300

    AuthorRepository was extended with CrudRepository

--- src/main/java/com/syrisa/springlibrarydocker/repository/AuthorRepository.java
+++ src/main/java/com/syrisa/springlibrarydocker/repository/AuthorRepository.java
@@ -1,7 +1,9 @@
 package com.syrisa.springlibrarydocker.repository;
 
+import com.syrisa.springlibrarydocker.model.impl.Author;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AuthorRepository {
+public interface AuthorRepository extends CrudRepository<Author,Integer> {
 }
