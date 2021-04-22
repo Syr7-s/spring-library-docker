c650224623961a0a56db920fe55d83c6dbde90d9
Syr7-s <isasayar7@github.com>
2021-04-22 17:10:49 +0300

    findAll method was created

--- src/main/java/com/syrisa/springlibrarydocker/repository/AuthorRepository.java
+++ src/main/java/com/syrisa/springlibrarydocker/repository/AuthorRepository.java
@@ -1,10 +1,14 @@
 package com.syrisa.springlibrarydocker.repository;
 
 import com.syrisa.springlibrarydocker.model.impl.Author;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthorRepository extends CrudRepository<Author, Integer> {
     Author findAuthorByAuthorName(String authorName);
+
+    Page<Author> findAll(Pageable pageable);
 }
