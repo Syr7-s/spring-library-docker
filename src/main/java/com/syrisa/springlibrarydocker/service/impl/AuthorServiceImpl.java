97d9082d1b1be938303c977519aa3cb8179fee38
Syr7-s <isasayar7@github.com>
2021-04-22 17:47:49 +0300

    AuthorServiceImpl was edited.

--- src/main/java/com/syrisa/springlibrarydocker/service/impl/AuthorServiceImpl.java
+++ src/main/java/com/syrisa/springlibrarydocker/service/impl/AuthorServiceImpl.java
@@ -1,7 +1,44 @@
 package com.syrisa.springlibrarydocker.service.impl;
 
+import com.syrisa.springlibrarydocker.model.impl.Author;
+import com.syrisa.springlibrarydocker.repository.AuthorRepository;
+import com.syrisa.springlibrarydocker.service.AuthorService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AuthorServiceImpl {
+public class AuthorServiceImpl implements AuthorService {
+    private final AuthorRepository authorRepository;
+
+    public AuthorServiceImpl(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+
+    @Override
+    public Author create(Author author) {
+        return authorRepository.save(author);
+    }
+
+    @Override
+    public Author update(Author author) {
+        return null;
+    }
+
+    @Override
+    public Page<Author> getAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Author getByAuthorName(String authorName) {
+        return null;
+    }
+
+    @Override
+    public String delete(int authorId) {
+        return null;
+    }
+
 }
