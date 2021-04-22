27a339e9099246ff1b2909f10135c25ded69904f
Syr7-s <isasayar7@github.com>
2021-04-22 17:05:44 +0300

    getByAuthorName and delete method were added.

--- src/main/java/com/syrisa/springlibrarydocker/service/AuthorService.java
+++ src/main/java/com/syrisa/springlibrarydocker/service/AuthorService.java
@@ -1,4 +1,9 @@
 package com.syrisa.springlibrarydocker.service;
 
-public interface AuthorService {
+import com.syrisa.springlibrarydocker.model.impl.Author;
+
+public interface AuthorService extends ModelService<Author> {
+    Author getByAuthorName(String authorName);
+
+    String delete(int authorId);
 }
