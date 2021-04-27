29101786e52fc4dfb4eed2235274ec74953bc769
Syr7-s <isasayar7@github.com>
2021-04-27 17:30:45 +0300

    BookRepository interface was extended from CrudRepository interface

--- src/main/java/com/syrisa/springlibrarydocker/repository/BookRepository.java
+++ src/main/java/com/syrisa/springlibrarydocker/repository/BookRepository.java
@@ -1,7 +1,9 @@
 package com.syrisa.springlibrarydocker.repository;
 
+import com.syrisa.springlibrarydocker.model.impl.Book;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository {
+public interface BookRepository extends CrudRepository<Book,Long> {
 }
