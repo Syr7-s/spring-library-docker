90f6a59558882187f021db5bf5a149ec2f01200e
Syr7-s <isasayar7@github.com>
2021-04-27 17:35:42 +0300

    findBookByBookName and findAll methods were added to the BookRepository

--- src/main/java/com/syrisa/springlibrarydocker/repository/BookRepository.java
+++ src/main/java/com/syrisa/springlibrarydocker/repository/BookRepository.java
@@ -1,9 +1,15 @@
 package com.syrisa.springlibrarydocker.repository;
 
+
 import com.syrisa.springlibrarydocker.model.impl.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends CrudRepository<Book,Long> {
+public interface BookRepository extends CrudRepository<Book, Long> {
+    Book findBookByBookName(String bookName);
+
+    Page<Book> findAll(Pageable pageable);
 }
