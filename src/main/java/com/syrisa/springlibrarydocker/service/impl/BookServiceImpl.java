e8b0c3654fe43010e88f5f605408100c76d171e8
Syr7-s <isasayar7@github.com>
2021-04-27 17:39:47 +0300

    @Transactional and @Service annotations were added.

--- src/main/java/com/syrisa/springlibrarydocker/service/impl/BookServiceImpl.java
+++ src/main/java/com/syrisa/springlibrarydocker/service/impl/BookServiceImpl.java
@@ -1,4 +1,9 @@
 package com.syrisa.springlibrarydocker.service.impl;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service
 public class BookServiceImpl {
 }
