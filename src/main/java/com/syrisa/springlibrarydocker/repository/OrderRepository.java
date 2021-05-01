a9048518dd3f234c75845473bcc30a38388e4a65
Syr7-s <isasayar7@github.com>
2021-05-01 14:38:34 +0300

    findAll method made generic.

--- src/main/java/com/syrisa/springlibrarydocker/repository/OrderRepository.java
+++ src/main/java/com/syrisa/springlibrarydocker/repository/OrderRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends CrudRepository<Orders, Integer> {
-    Page findAll(PageRequest pageRequest);
+    Page<Orders> findAll(PageRequest pageRequest);
 
 }
