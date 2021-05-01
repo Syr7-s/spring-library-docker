370539d61ba6680e7640bc6015b82d6b3b78be87
Syr7-s <isasayar7@github.com>
2021-05-01 14:43:13 +0300

    OrderServiceImpl was implemented from OrderService named interface.

--- src/main/java/com/syrisa/springlibrarydocker/service/impl/OrderServiceImpl.java
+++ src/main/java/com/syrisa/springlibrarydocker/service/impl/OrderServiceImpl.java
@@ -1,7 +1,36 @@
 package com.syrisa.springlibrarydocker.service.impl;
 
+import com.syrisa.springlibrarydocker.model.Model;
+import com.syrisa.springlibrarydocker.model.impl.Orders;
+import com.syrisa.springlibrarydocker.service.OrderService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
-public class OrderServiceImpl {
+public class OrderServiceImpl implements OrderService {
+    @Override
+    public Model create(Model model) {
+        return null;
+    }
+
+    @Override
+    public Model update(Model model) {
+        return null;
+    }
+
+    @Override
+    public Page getAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Orders getOrdersByOrdersId(int id) {
+        return null;
+    }
+
+    @Override
+    public String delete(int ordersId) {
+        return null;
+    }
 }
