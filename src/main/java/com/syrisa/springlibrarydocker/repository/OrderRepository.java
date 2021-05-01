42864a711961a3bceb729786787d8941cd62d0f9
Syr7-s <isasayar7@github.com>
2021-05-01 14:37:37 +0300

    findAll method was added to the OrderRepository

--- src/main/java/com/syrisa/springlibrarydocker/repository/OrderRepository.java
+++ src/main/java/com/syrisa/springlibrarydocker/repository/OrderRepository.java
@@ -1,7 +1,13 @@
 package com.syrisa.springlibrarydocker.repository;
 
+import com.syrisa.springlibrarydocker.model.impl.Orders;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository {
+public interface OrderRepository extends CrudRepository<Orders, Integer> {
+    Page findAll(PageRequest pageRequest);
+
 }
