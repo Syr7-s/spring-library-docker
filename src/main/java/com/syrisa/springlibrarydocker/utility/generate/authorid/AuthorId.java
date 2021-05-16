0b29de973a1252edbbfaaf339d83127c3cf23b98
Syr7-s <isasayar7@github.com>
2021-05-16 17:33:59 +0300

    AuthorId and CredentialNumber were edited.

--- src/main/java/com/syrisa/springlibrarydocker/utility/generate/authorid/AuthorId.java
+++ src/main/java/com/syrisa/springlibrarydocker/utility/generate/authorid/AuthorId.java
@@ -3,20 +3,11 @@ package com.syrisa.springlibrarydocker.utility.generate.authorid;
 import com.syrisa.springlibrarydocker.utility.generate.NumberGenerate;
 
 import java.util.Random;
-import java.util.function.Supplier;
 
 public final class AuthorId {
     private AuthorId() {
     }
 
-    /* public static Supplier<Long> authorIdGenerate = () -> {
-         StringBuilder builder = new StringBuilder();
-         builder.append(1 + new Random().nextInt(9));
-         for (int i = 0; i < 16; i++) {
-             builder.append(new Random().nextInt(10));
-         }
-         return Long.parseLong(builder.toString());
-     };*/
     public static final NumberGenerate numberGenerate = numLength -> {
         StringBuilder builder = new StringBuilder();
         builder.append(10);
--- src/main/java/com/syrisa/springlibrarydocker/utility/generate/credentialnumber/CredentialNumber.java
+++ src/main/java/com/syrisa/springlibrarydocker/utility/generate/credentialnumber/CredentialNumber.java
@@ -3,21 +3,11 @@ package com.syrisa.springlibrarydocker.utility.generate.credentialnumber;
 import com.syrisa.springlibrarydocker.utility.generate.NumberGenerate;
 
 import java.util.Random;
-import java.util.function.Supplier;
 
 public final class CredentialNumber {
     private CredentialNumber() {
 
     }
-
-   /* public static final Supplier<Long> generateNumber = () -> {
-        StringBuilder builder = new StringBuilder();
-        builder.append(1 + new Random().nextInt(9));
-        for (int i = 0; i < 10; i++) {
-            builder.append(new Random().nextInt(10));
-        }
-        return Long.parseLong(builder.toString());
-    };*/
 
     public static final NumberGenerate numberGenerate =  numLength -> {
         StringBuilder builder = new StringBuilder();
